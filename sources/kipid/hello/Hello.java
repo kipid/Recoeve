0e8963f0f301a66192ed9ca4739e65f5a3d7c533
kipid <kipacti@gmail.com>
2017-08-04 10:07:34 +0900

    Style changed.

--- sources/kipid/hello/Hello.java
+++ sources/kipid/hello/Hello.java
@@ -2,8 +2,8 @@ package kipid.hello;
 
 import static java.lang.Math.*;
 
-class Hello{
-	public static void main(String... args){
+class Hello {
+	public static void main(String... args) {
 		System.out.println("Hello World! 한글!");
 		for (int i=0; i<args.length; i++) {
 			System.out.println("Hello "+args[i]);
--- sources/kipid/hello/Hello1.java
+++ sources/kipid/hello/Hello1.java
@@ -1,7 +1,7 @@
 package kipid.hello;
 
-class Hello1{
-	public static void main(String... args){
+class Hello1 {
+	public static void main(String... args) {
 		Hello.main(args);
 		System.out.println("Hello1 World! 한글."); // 한글 comment.
 	}
