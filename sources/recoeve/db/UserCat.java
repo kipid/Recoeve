69b8e44e642a0e390aff388206bf4d215cc4302c
Kang-Soo Lee <kipacti@gmail.com>
2022-12-24 10:01:17 +0900

    class UserCat.java

---
+++ sources/recoeve/db/UserCat.java
@@ -0,0 +1,34 @@
+package recoeve.db;
+
+import java.util.*;
+
+public class UserCat {
+	public long user_i;
+	public String cat_i;
+
+	private static final int prime=31;
+
+	@Override
+	public int hashCode() {
+		int result=1;
+		result=prime*result+(int)user_i;
+		result=prime*result+((cat_i==null)?0:cat_i.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this==obj) {return true;}
+		if (obj==null) {return false;}
+		if (getClass()!=obj.getClass()) {return false;}
+		UserCat other=(UserCat)obj;
+		if (user_i!=other.user_i) {return false;}
+		if (cat_i==null) {
+			if (other.cat_i!=null) {return false;}
+		} else if (!cat_i.equals(other.cat_i)) {return false;}
+		return true;
+	}
+
+	public static void main(String... args) {
+	}
+}
