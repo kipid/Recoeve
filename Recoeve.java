c142f09bc8c0ad8ab06e1a5e89b8afc60c411302
Kang-Soo Lee <kipacti@gmail.com>
2023-06-03 23:17:49 +0900

    Update Recoeve.java

--- Recoeve.java
+++ Recoeve.java
@@ -39,7 +39,7 @@ import recoeve.db.StrArray;
 
 
 public class Recoeve extends AbstractVerticle {
-	public static final String host
+	public static final String HOST
 		// ="recoeve.net";
 		// ="0.0.0.0";
 		="localhost";
@@ -743,7 +743,12 @@ public void start() {
 				.setPath("C:/Recoeve/recoeve.jks")
 				.setPassword("Kd8#j$LL0@OM1")
 			)
-	).requestHandler(router).listen(443, Recoeve.host); //
+	).requestHandler(router).listen(443);
+	// UnderConstruction.HOST
+	// "43.200.166.14" : 탄력적 IP
+	// "43.200.166.14" : 퍼블릭 IPv4 주소
+	// "ec2-43-200-166-14.ap-northeast-2.compute.amazonaws.com" : 퍼블릭 IPv4 DNS
+	// "172.31.35.249" : 프라이빗 IPv4 주소
 	vertx.createHttpServer()
 		.requestHandler(router).listen(80);
 } // public void start()
