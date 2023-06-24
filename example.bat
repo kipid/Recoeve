95ba939a5f5d6e1724526fa3aea893293005b0c5
Kang-Soo Lee <kipacti@gmail.com>
2023-06-25 06:40:00 +0900

    Create example.bat

---
+++ example.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+@rem Set local scope for the variables with windows NT shell
+echo OS=%OS%
+if "%OS%"=="Windows_NT" setlocal
+echo Hello, World!
+echo This is kipid.
