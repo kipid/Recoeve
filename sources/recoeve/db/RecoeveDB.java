93759e534a65bd9e6de11d71e543a92a2e291954
Kang-Soo Lee <kipacti@gmail.com>
2023-09-14 13:55:01 +0900

    iter.remove().

--- sources/recoeve/db/RecoeveDB.java
+++ sources/recoeve/db/RecoeveDB.java
@@ -2384,11 +2384,13 @@ public void deleteCatsUriFromList(long user_me, Categories cats, String uri, Cat
 	}
 }
 public void catsChangedOnUri(long user_me, Categories oldCats, Categories newCats, String uri, CatList catL, boolean equalityOfValuesOfPts, Points oldPts, Points newPts, Timestamp tNow) throws SQLException {
-	oldCats.setOfCats.forEach((oldCat) -> {
+	Iterator<String> iter=oldCats.setOfCats.iterator();
+	while (iter.hasNext()) {
+		String oldCat=iter.next();
 		if (newCats.setOfCats.remove(oldCat)) {
-			oldCats.setOfCats.remove(oldCat);
+			iter.remove();
 		}
-	});
+	}
 	deleteCatsUriFromList(user_me, oldCats, uri, catL);
 	putCatsUriToList(user_me, newCats, uri, catL);
 	updateDefCat(uri, oldCats, -1);
