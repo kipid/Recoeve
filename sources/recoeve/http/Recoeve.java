489c634e93faf65ffbf2c65b634716b916f66fea
Kang-Soo Lee <kipacti@gmail.com>
2023-10-03 10:05:16 +0900

    cmt removed.

--- sources/recoeve/http/Recoeve.java
+++ sources/recoeve/http/Recoeve.java
@@ -4,7 +4,6 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
-// import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.http.HttpServerRequest;
