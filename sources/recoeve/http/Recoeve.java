18c63cce4652daa451992b0a263c64625f9767a5
Kang-Soo Lee <kipacti@gmail.com>
2023-11-02 09:51:56 +0900

    "ETag", "1.2.2"

--- sources/recoeve/http/Recoeve.java
+++ sources/recoeve/http/Recoeve.java
@@ -188,7 +188,7 @@ public static void main(String... args) {
 			}
 			if (fileName!=null&&!fileName.isEmpty()) {
 				pl.req.response().putHeader("Cache-Control", "public, max-age=86400, immutable"); // 1 Day=86400 sec.
-				pl.req.response().putHeader("ETag", "1.2.1");
+				pl.req.response().putHeader("ETag", "1.2.2");
 				String[] fileNameSplit=fileName.split("\\.");
 				switch (fileNameSplit[fileNameSplit.length-1]) {
 					case "ico":
