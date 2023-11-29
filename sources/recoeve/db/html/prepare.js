4de8bfc8a44313e065b21f8470df146d215f56ba
Kang-Soo Lee <kipacti@gmail.com>
2023-11-29 16:27:37 +0900

    back to.

--- sources/recoeve/db/html/prepare.js
+++ sources/recoeve/db/html/prepare.js
@@ -472,18 +472,12 @@ m.strToJSON=function (str, colMap=true, rowMap=false) {
 	}
 	if (colMap) {
 		const firstColSize=ret[0].length;
-		for (let j=0;j<firstColSize;j++) {
-			let key=String(ret[0][j]);
-			ret[0][key]=String(ret[0][j]);
-			console.log(ret[0][j]);
-		}
-		for (let i=1;i<ret.length;i++) {
-			let jMax=(ret[i].length>firstColSize?firstColSize:ret[i].length);
+		for (let i=0;i<ret.length;i++) {
+			let jMax=ret[i].length>firstColSize?firstColSize:ret[i].length;
 			for (let j=0;j<firstColSize;j++) {
-				let key=String(ret[0][j]);
+				let key=ret[0][j];
 				if (j<jMax) {
-					ret[i][key]=String(ret[i][j]);
-					console.log(ret[i][j]);
+					ret[i][key]=ret[i][j];
 				}
 				else {
 					ret[i][key]="";
