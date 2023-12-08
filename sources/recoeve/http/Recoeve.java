40036347b748e4b00d43bfa37f274dc4133e4c5b
Kang-Soo Lee <kipacti@gmail.com>
2023-12-08 13:03:20 +0900

    table CSS, and "ETag", "1.5.3"

--- sources/recoeve/http/Recoeve.java
+++ sources/recoeve/http/Recoeve.java
@@ -205,7 +205,7 @@ public static void main(String... args) {
 			}
 			if (fileName!=null&&!fileName.isEmpty()) {
 				pl.req.response().putHeader("Cache-Control", "public, max-age=86400, immutable"); // 1 Day=86400 sec.
-				pl.req.response().putHeader("ETag", "1.5.2");
+				pl.req.response().putHeader("ETag", "1.5.3");
 				String[] fileNameSplit=fileName.split("\\.");
 				switch (fileNameSplit[fileNameSplit.length-1]) {
 					case "ico":
