438cc7f8f7fefeda10c99508b66b41ab42e5838e
Kang-Soo Lee <kipacti@gmail.com>
2024-02-04 23:01:33 +0900

    "ETag", "1.6.1"

--- src/main/java/recoeve/http/Recoeve.java
+++ src/main/java/recoeve/http/Recoeve.java
@@ -295,7 +295,7 @@ public class Recoeve extends AbstractVerticle {
 				if (fileName != null && !fileName.isEmpty()) {
 					pl.req.response().putHeader("Cache-Control", "public, max-age=86400, immutable"); // 1 Day=86400
 																										// sec.
-					pl.req.response().putHeader("ETag", "1.6.0");
+					pl.req.response().putHeader("ETag", "1.6.1");
 					String[] fileNameSplit = fileName.split("\\.");
 					switch (fileNameSplit[fileNameSplit.length - 1]) {
 						case "ico":
