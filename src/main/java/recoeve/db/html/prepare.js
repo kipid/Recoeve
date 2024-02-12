24b27885fcfcfd7aff5cff2570f9440531a39bb8
Kang-Soo Lee <kipacti@gmail.com>
2024-02-12 14:13:37 +0900

    async

--- src/main/java/recoeve/db/html/prepare.js
+++ src/main/java/recoeve/db/html/prepare.js
@@ -1232,7 +1232,7 @@ ${String(recoDef.heads[1]?.naver).trim() && String(recoDef.heads[1]?.naver) !== 
 		}
 	};
 	m.formatURIFully = async function (uri, uriRendered) {
-		return new Promise(function (resolve, reject) {
+		return new Promise(async function (resolve, reject) {
 			let from = String(uriRendered.from);
 			console.log(`uriRendered`, uriRendered, `uriRendered.from: ${uriRendered.from}\nuri: ${uri}`);
 			m.lastURI = uri;
