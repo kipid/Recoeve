9bc4e12c8c4c7c3644f55b7c1b7e07a76a7401db
kipid <kipacti@google.com>
2024-08-17 07:58:19 +0900

    Apply .gitignore

--- gitconfig.bat
+++ gitconfig.bat
@@ -1,5 +1,5 @@
 rem copy to C:\kipid\PortableGit\bin
-git config alias.history "log --pretty=oneline"
-git config user.name "kipid"
-git config user.email "kipacti@gmail.com"
+git config --global alias.history "log --pretty=oneline"
+git config --global user.name "kipid"
+git config --global user.email "kipacti@gmail.com"
 git config --global init.defaultBranch main
