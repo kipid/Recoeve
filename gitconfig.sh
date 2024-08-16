67d8678cfc52d495dea8c7494c973c6cf94f361d
kipid <kipacti@google.com>
2024-08-16 04:22:35 +0000

    Copy gitconfig.sh from ~/.config/gitconfig.sh

---
+++ gitconfig.sh
@@ -0,0 +1,9 @@
+sudo git config --global core.editor "vim"
+export GIT_EDITOR=vim
+export VISUAL=vim
+export EDITOR="$VISUAL"
+sudo git config alias.history "log --pretty=oneline" --global
+sudo git config user.name "kipid" --global
+sudo git config user.email "kipacti@google.com" --global
+sudo git config --global init.defaultBranch main --global
+
