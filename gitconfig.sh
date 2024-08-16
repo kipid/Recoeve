c01c06f9e1ff5685f78de50d57e77c2e0da2fac8
kipid <kipacti@google.com>
2024-08-16 17:39:32 +0900

    Update gitconfig.sh

--- gitconfig.sh
+++ gitconfig.sh
@@ -3,7 +3,7 @@
 
 cd ~/Recoeve/
 
-sudo git config --global core.editor "vim"
+sudo git config core.editor "vim" --global
 export GIT_EDITOR=vim
 export VISUAL=vim
 export EDITOR="$VISUAL"
@@ -11,7 +11,7 @@ export EDITOR="$VISUAL"
 sudo git config alias.history "log --pretty=oneline" --global
 sudo git config user.name "kipid" --global
 sudo git config user.email "kipacti@gmail.com" --global
-sudo git config --global init.defaultBranch main --global
+sudo git config init.defaultBranch main --global
 sudo git config pull.rebase true --global
 
 eval "$(ssh-agent -s)"
