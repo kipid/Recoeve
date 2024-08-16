ca16b19a3142cdacf26a8bae2ce1d25f71cfffdb
kipid <kipacti@google.com>
2024-08-17 08:57:53 +0900

    Update gitconfig.bat

--- gitconfig.bat
+++ gitconfig.bat
@@ -1,5 +1,12 @@
 rem copy to C:\kipid\PortableGit\bin
+
+git config --global core.editor "vim"
+export GIT_EDITOR=vim
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
 git config --global alias.history "log --pretty=oneline"
 git config --global user.name "kipid"
 git config --global user.email "kipacti@gmail.com"
 git config --global init.defaultBranch main
+git config --global pull.rebase true
