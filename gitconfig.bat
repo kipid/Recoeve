9e3c78fa08fa9301cb75df31104eaa83cbd6a8eb
kipid <kipacti@google.com>
2024-08-16 15:54:37 +0900

    Update gitconfig.bat

--- gitconfig.bat
+++ gitconfig.bat
@@ -1,7 +1,5 @@
+rem copy to C:\kipid\PortableGit\bin
 git config alias.history "log --pretty=oneline"
-
 git config user.name "kipid"
-
-git config user.email "kipacti@google.com"
-
+git config user.email "kipacti@gmail.com"
 git config --global init.defaultBranch main
