dbcac031c5e6bdbbfb9f25381c7883335788bac4
kipid <kipacti@gmail.com>
2024-08-18 09:54:36 +0900

    Update gitconfig.bat

--- gitconfig.bat
+++ gitconfig.bat
@@ -1,15 +1,17 @@
 rem copy to C:\kipid\PortableGit\bin
 
+:: Current directory
+SET SD=%CD%
+
 cd C:/Recoeve/
 
 git config --global core.editor "vim"
-export GIT_EDITOR=vim
-export VISUAL=vim
-export EDITOR="$VISUAL"
-
 git config --global alias.history "log --pretty=oneline"
 git config --global user.name "kipid"
 git config --global user.email "kipacti@gmail.com"
-git config --global init.defaultBranch main
+git config --global --replace-all init.defaultBranch main
 git config --global pull.rebase true
 git pull
+
+:: Back to current directory
+CD %SD%
