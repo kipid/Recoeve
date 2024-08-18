e348656ed6f2dfe333b4928d8da23f394f28e028
kipid <kipacti@gmail.com>
2024-08-18 15:08:27 +0900

    Update gitconfig.sh

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,6 +1,8 @@
 # sudo vim ~/.bashrc
 # cp ~/Recoeve/gitconfig.sh ~/.config/
 
+BASEDIR=${PWD}
+
 cd ~/Recoeve/
 
 sudo git config --global core.editor "vim"
@@ -14,9 +16,15 @@ sudo git config --global user.email "kipacti@gmail.com"
 sudo git config --global --replace-all init.defaultBranch main
 sudo git config --global pull.rebase true
 
+eval "$(ssh-agent -s)"
+sudo ssh-add ~/RecoeveNet/.ssh/github-recoeve-rsa
+sudo git config --global credential.helper cache
+
+sudo git remote set-url origin git@github.com:kipid/Recoeve.git
 sudo git pull origin main
 
 cd ~/RecoeveNet/
+sudo git remote set-url origin git@github.com:kipid/RecoeveNet.git
 sudo git pull origin main
 
-cd ~/Recoeve/
+cd ${BASEDIR}
