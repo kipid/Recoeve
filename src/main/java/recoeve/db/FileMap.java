eede8e082bcbca4d636c24bb61ca5b6c2ec8f649
kipid <kipacti@gmail.com>
2024-08-20 21:55:29 +0900

    Update FileMap.java

--- src/main/java/recoeve/db/FileMap.java
+++ src/main/java/recoeve/db/FileMap.java
@@ -97,7 +97,7 @@ public class FileMap {
 	// txtFileMap.get("txtFileName").get("lang")
 	public static StrArray langMap;
 
-	public static final Pattern ptnReplacer = Pattern.compile("\\[--[^\\[\\]]+--\\]");
+	public static final Pattern ptnReplacer = Pattern.compile("\\[--[^\\[\\]]+?--\\]");
 
 	static {
 		refererSet = new HashSet<String>();
