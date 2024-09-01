4cc689329cfb25c972f6438ca2a373b4d0a013f7
kipid <kipacti@gmail.com>
2024-09-01 15:29:33 +0900

    Update docuK-postProcess-2.3.js

--- src/main/java/recoeve/db/CDN/docuK-postProcess-2.3.js
+++ src/main/java/recoeve/db/CDN/docuK-postProcess-2.3.js
@@ -668,6 +668,7 @@ Log <span class="bold underline">o</span>ut
 			window?.MathJax?.typeset?.([$(".comments")[0]]);
 			window?.PR?.prettyPrint?.();
 		}
+		m.reNewAndReOn();
 	};
 
 
@@ -678,8 +679,7 @@ Log <span class="bold underline">o</span>ut
 
 	m.reNewAndReOn = function () {
 		m.$delayedElems = $("[delayed-src], [delayed-bgimage], .to-be-executed");
-		m.$window.off("scroll.delayedLoad");
-		m.$window.on("scroll.delayedLoad", m.delayedLoadByScroll);
+		m.$window.off("scroll.delayedLoad").on("scroll.delayedLoad", m.delayedLoadByScroll);
 		m.$window.trigger("scroll.delayedLoad");
 		m.$fdList = $("#header, #shortkey, .promoting, .change-docuK-style, #content, #container, #wrapContent, .docuK .sec>h1, .docuK .sec>h2, .docuK .subsec>h3, .docuK .subsubsec>h4, .comments, .comments>.comment-list>ul>li, #disqus_thread, #aside, #page-views-chart, #chartdiv, #recentComments, #tistorySidebarProfileLayer");
 	};
