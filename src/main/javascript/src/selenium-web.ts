6f00d24b73a819915d738608b6a641c66cf193b4
kipid <kipacti@gmail.com>
2024-09-03 00:40:18 +0900

    Update selenium-web.ts

--- src/main/javascript/src/selenium-web.ts
+++ src/main/javascript/src/selenium-web.ts
@@ -1,24 +1,24 @@
-import { Builder, Browser, By, Key, until, WebDriver, WebElement } from 'selenium-webdriver';
-import { ChromiumWebDriver } from 'selenium-webdriver/chromium';
+import { Browser, Builder, By, WebDriver, WebElement } from "selenium-webdriver";
 
 (async function example(): Promise<void> {
-  let driver:WebDriver = await new Builder().forBrowser(Browser.CHROME).build();
-  try {
-    driver.get('https://kipid.tistory.com/entry/Lists')
-      .then(function findHeads() {
-        const heads:Promise<WebElement[]> = driver.findElements(By.css('title, h1, h2'));
-        heads.then((heads:WebElement[]) => {
-          if (!heads.some(async (head:WebElement) => {
-              let headText:string = await head.getText();
-              headText = headText.replace(/\s/g, " ").trim();
-              console.log(headText);
-              return !!headText.length;
-            })) {
-            setTimeout(findHeads, 200);
-          }
-        });
-      })
-  } finally {
-    driver.quit();
-  }
+	let driver: WebDriver = await new Builder().forBrowser(Browser.CHROME).build();
+	try {
+		await driver.get('https://kipid.tistory.com/entry/Lists');
+		await findHeads(driver);
+	} finally {
+		await driver.quit();
+	}
 })();
+
+async function findHeads(driver: WebDriver): Promise<void> {
+	const heads: WebElement[] = await driver.findElements(By.css('title, h1, h2'));
+	for (const head of heads) {
+		let headText: string = await head.getText();
+		headText = headText.replace(/\s/g, ' ').trim();
+		console.log(headText);
+		if (headText.length) {
+			return; // Exit the loop if we find a non-empty headText
+		}
+	}
+	setTimeout(() => findHeads(driver), 200);
+}
