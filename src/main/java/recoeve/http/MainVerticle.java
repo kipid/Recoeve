56500e1f95cbbd93a0afca5b7022a4a630c6e329
kipid <kipacti@gmail.com>
2024-09-23 13:41:09 +0900

    Update MainVerticle.java

--- src/main/java/recoeve/http/MainVerticle.java
+++ src/main/java/recoeve/http/MainVerticle.java
@@ -5,13 +5,10 @@ package recoeve.http;
 import java.sql.SQLException;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
-import io.vertx.core.json.JsonObject;
 import recoeve.db.FileMap;
 import recoeve.db.FileMapWithVar;
 import recoeve.db.RecoeveDB;
@@ -40,56 +37,56 @@ public class MainVerticle extends AbstractVerticle {
 	@Override
 	public void start(Promise<Void> startPromise) {
 		LOG.info("Start " + getClass().getName() + " on tread " + Thread.currentThread().getName());
-		JsonObject config = new JsonObject().put("maxDrivers", RecoeveWebClient.DEFAULT_MAX_DRIVERS);
-		Future<String> deploy0 = vertx.deployVerticle(recoeveWebClient, new DeploymentOptions(config).setInstances(1))
-			.onComplete(res -> {
-				if (res.succeeded()) {
-					verticleId0 = res.result();
-					System.out.println("Deployment id is: " + verticleId0);
-					LOG.info("RecoeveWebClient is deployed.");
-				}
-				else {
-					System.out.println("Deployment failed!");
-					LOG.error("RecoeveWebClient is NOT deployed.");
-				}
-			});
-		Future<String> deploy1 = vertx.deployVerticle(recoeve, new DeploymentOptions().setInstances(1))
-			.onComplete(res -> {
-				if (res.succeeded()) {
-					verticleId1 = res.result();
-					System.out.println("Deployment id is: " + verticleId1);
-					LOG.info("Recoeve is deployed.");
-				}
-				else {
-					System.out.println("Deployment failed!");
-					LOG.error("Recoeve is NOT deployed.");
-				}
-			});
-		Future.all(deploy0, deploy1).onComplete((res) -> {
-			startPromise.complete();
-		});
+		// JsonObject config = new JsonObject().put("maxDrivers", RecoeveWebClient.DEFAULT_MAX_DRIVERS);
+		// Future<String> deploy0 = vertx.deployVerticle(recoeveWebClient, new DeploymentOptions(config).setInstances(1))
+		// 	.onComplete(res -> {
+		// 		if (res.succeeded()) {
+		// 			verticleId0 = res.result();
+		// 			System.out.println("Deployment id is: " + verticleId0);
+		// 			LOG.info("RecoeveWebClient is deployed.");
+		// 		}
+		// 		else {
+		// 			System.out.println("Deployment failed!");
+		// 			LOG.error("RecoeveWebClient is NOT deployed.");
+		// 		}
+		// 	});
+		// Future<String> deploy1 = vertx.deployVerticle(recoeve, new DeploymentOptions().setInstances(1))
+		// 	.onComplete(res -> {
+		// 		if (res.succeeded()) {
+		// 			verticleId1 = res.result();
+		// 			System.out.println("Deployment id is: " + verticleId1);
+		// 			LOG.info("Recoeve is deployed.");
+		// 		}
+		// 		else {
+		// 			System.out.println("Deployment failed!");
+		// 			LOG.error("Recoeve is NOT deployed.");
+		// 		}
+		// 	});
+		// Future.all(deploy0, deploy1).onComplete((res) -> {
+		// 	startPromise.complete();
+		// });
 	}
 
 	@Override
 	public void stop(Promise<Void> stopPromise) {
-		Future<Void> undeploy0 = vertx.undeploy(verticleId0)
-			.onComplete(res -> {
-				if (res.succeeded()) {
-					System.out.println("Undeployed ok.");
-				}
-				else {
-					System.out.println("Undeploy failed!");
-				}
-			});
-		Future<Void> undeploy1 = vertx.undeploy(verticleId1)
-			.onComplete(res -> {
-				if (res.succeeded()) {
-					System.out.println("Undeployed ok.");
-				}
-				else {
-					System.out.println("Undeploy failed!");
-				}
-			});
+		// Future<Void> undeploy0 = vertx.undeploy(verticleId0)
+		// 	.onComplete(res -> {
+		// 		if (res.succeeded()) {
+		// 			System.out.println("Undeployed ok.");
+		// 		}
+		// 		else {
+		// 			System.out.println("Undeploy failed!");
+		// 		}
+		// 	});
+		// Future<Void> undeploy1 = vertx.undeploy(verticleId1)
+		// 	.onComplete(res -> {
+		// 		if (res.succeeded()) {
+		// 			System.out.println("Undeployed ok.");
+		// 		}
+		// 		else {
+		// 			System.out.println("Undeploy failed!");
+		// 		}
+		// 	});
 		context = null;
 		vertx = null;
 		fileMap = null;
@@ -104,7 +101,7 @@ public class MainVerticle extends AbstractVerticle {
 			System.out.println("SQLException: " + err.getMessage());
 		}
 		db = null;
-		Future.all(undeploy0, undeploy1).onComplete(res -> {
+		// Future.all(undeploy0, undeploy1).onComplete(res -> {
 			recoeve.httpServer.close(ar -> {
 				if (ar.succeeded()) {
 					System.out.println("HTTP server closed successfully.");
@@ -116,7 +113,7 @@ public class MainVerticle extends AbstractVerticle {
 				stopPromise.complete();
 			});
 			recoeve = null;
-		});
+		// });
 	}
 
 	public static void main(String... args) {
